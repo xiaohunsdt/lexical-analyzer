fe4a0446b9e49e82e7dd02f4fa0b4ef0db089be6
xiaohun' pc <xiaohunsdt@gmail.com>
2019-04-15 02:46:34 +0900

    update

--- src/main/java/net/novaborn/fa/hander/ExpToNfaHandler.java
+++ src/main/java/net/novaborn/fa/hander/ExpToNfaHandler.java
@@ -2,7 +2,9 @@ package net.novaborn.fa.hander;
 
 import net.novaborn.fa.entity.NFA;
 import net.novaborn.fa.entity.State;
+import net.novaborn.fa.entity.Transition;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Stack;
 
@@ -24,14 +26,29 @@ public class ExpToNfaHandler implements BaseHandler {
         nfa.addState(startState);
 
         //conversion reg to states of nfa
-        Stack<Character> opreation = new Stack<>();
-        for(Character character : expression.toCharArray()){
-            if(character.equals("(")){
-                opreation.push(character);
-            }else if(character.equals(")")) {
-                opreation.clear();
-            }else if(character.equals("*")){
+        this.handle(expression, startState);
 
+        return this;
+    }
+
+    private BaseHandler handle(String regularExp, State previousState) {
+        //conversion reg to states of nfa
+        Stack<Character> opreation = new Stack<>();
+        Stack<State> stateStack = new Stack<>();
+        for (Character character : expression.toCharArray()) {
+            if (character.equals("(")) {
+                opreation.push(character);
+            } else if (character.equals(")")) {
+                opreation.clear();
+            } else if (character.equals("*")) {
+                State firstState = stateStack.firstElement();
+            } else {
+                State thisState = new State(previousState.getId() + 1);
+                Transition transition = new Transition();
+                transition.setCharacters(Arrays.asList(character));
+                transition.setFromState(previousState);
+                transition.setToState(thisState);
+                this.nfa.addTransitionFunc(transition);
             }
         }
 
